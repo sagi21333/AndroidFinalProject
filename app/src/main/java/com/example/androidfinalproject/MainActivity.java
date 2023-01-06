bb2d5bf9332cebd225870ab0975c868fb98b807b
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-01-06 17:40:50 +0200

    new nav graph with empty fragments to implementation

--- app/src/main/java/com/example/androidfinalproject/MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.androidfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    NavController navCtrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHost navHost = (NavHost) getSupportFragmentManager().findFragmentById(R.id.mainNavHost);
+        navCtrl = navHost.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navCtrl);
+
+//        if (Model.instance.isSignedIn()) {
+//            navCtrl.navigate(R.id.action_loginFragment_to_allReviewsFragment);
+//        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (!super.onOptionsItemSelected(item)) {
+            switch (item.getItemId()) {
+                case android.R.id.home:
+                    navCtrl.navigateUp();
+                    break;
+                default:
+                    NavigationUI.onNavDestinationSelected(item,navCtrl);
+            }
+        } else {
+            return true;
+        }
+        return false;
     }
 }
