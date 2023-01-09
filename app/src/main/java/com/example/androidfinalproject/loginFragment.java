81e68251f882d174ed377be8161868d869ca451b
asaf halely <asafhalely@gmail.com>
2023-01-09 18:34:36 +0200

    connect login fregment with registration

--- app/src/main/java/com/example/androidfinalproject/loginFragment.java
+++ app/src/main/java/com/example/androidfinalproject/loginFragment.java
@@ -3,62 +3,41 @@ package com.example.androidfinalproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link loginFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class loginFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public loginFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment loginFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static loginFragment newInstance(String param1, String param2) {
-        loginFragment fragment = new loginFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    EditText username;
+    EditText password;
+    Button confirmBtn;
+    TextView toRegister;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        username = (EditText) view.findViewById(R.id.login_username_edittxt);
+        password = (EditText) view.findViewById(R.id.login_password_edittxt);
+        confirmBtn = view.findViewById(R.id.login_login_btn);
+        toRegister = view.findViewById(R.id.login_signup_txtview);
+
+
+        toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getView()).navigate(R.id.action_loginFragment_to_registrationFragment);
+            }
+        });
+
+        return view;
     }
 }
