f8dbe5a38f49713b68ae7acc9c707830b0cf9620
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-01-14 11:43:42 +0200

    MovieReviewFragment get movie id

--- app/src/main/java/com/example/androidfinalproject/MovieReviewFragment.java
+++ app/src/main/java/com/example/androidfinalproject/MovieReviewFragment.java
@@ -12,36 +12,47 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.androidfinalproject.databinding.FragmentMovieReviewBinding;
+import com.example.androidfinalproject.databinding.FragmentReviewListBinding;
 import com.example.androidfinalproject.model.Model;
 import com.example.androidfinalproject.model.Movie;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
+import java.text.SimpleDateFormat;
+
 public class MovieReviewFragment extends Fragment {
 
-    ImageView movieImg;
-    TextView name;
-    TextView director;
-    TextView releasedate;
+//    ImageView movieImg;
+//    TextView name;
+//    TextView director;
+//    TextView releasedate;
 
-//    Bundle bundle = new Bundle();
-
+    FragmentMovieReviewBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_movie_review, container, false);
+
+        binding = FragmentMovieReviewBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
         String movieId = MovieReviewFragmentArgs.fromBundle(getArguments()).getMovieId();
         Movie movie = Model.instance.getMovieById(movieId);
 
-//        bundle.putString("movieId", movieId);
-//        FragmentContainerView fragmentContainerView = (FragmentContainerView) view.findViewById(R.id.fragmentContainerView);
-//        ReviewListFragment reviewListFragment = (ReviewListFragment) fragmentContainerView.getFragment();
-//
-//        if(reviewListFragment!=null) reviewListFragment.setArguments(bundle);
+        binding.movieReviewsNameTxt.setText(movie.getTitle());
 
+        if (movie.getMovieImageUrl() != null &&
+                !movie.getMovieImageUrl().equals("")) {
+            Picasso.get()
+                    .load(movie.getMovieImageUrl())
+                    .into(binding.movieReviewsImg);
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        binding.movieReviewsReleasedateTxt.setText(dateFormat.format(movie.getReleaseDate()));
 
         return view;
 
