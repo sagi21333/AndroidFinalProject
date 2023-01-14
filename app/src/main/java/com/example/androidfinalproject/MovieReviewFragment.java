2e829574e60cc5f7066309653c1f09170dc118fe
Sagi Ben Mordechai <79403955+sagi21333@users.noreply.github.com>
2023-01-14 11:30:42 +0200

    add navigate from movies list to movie page

--- app/src/main/java/com/example/androidfinalproject/MovieReviewFragment.java
+++ app/src/main/java/com/example/androidfinalproject/MovieReviewFragment.java
@@ -1,64 +1,50 @@
 package com.example.androidfinalproject;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MovieReviewFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.androidfinalproject.model.Model;
+import com.example.androidfinalproject.model.Movie;
+
+import org.w3c.dom.Text;
+
 public class MovieReviewFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    ImageView movieImg;
+    TextView name;
+    TextView director;
+    TextView releasedate;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+//    Bundle bundle = new Bundle();
 
-    public MovieReviewFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MovieReviewFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MovieReviewFragment newInstance(String param1, String param2) {
-        MovieReviewFragment fragment = new MovieReviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_review, container, false);
+        View view = inflater.inflate(R.layout.fragment_movie_review, container, false);
+
+        String movieId = MovieReviewFragmentArgs.fromBundle(getArguments()).getMovieId();
+        Movie movie = Model.instance.getMovieById(movieId);
+
+//        bundle.putString("movieId", movieId);
+//        FragmentContainerView fragmentContainerView = (FragmentContainerView) view.findViewById(R.id.fragmentContainerView);
+//        ReviewListFragment reviewListFragment = (ReviewListFragment) fragmentContainerView.getFragment();
+//
+//        if(reviewListFragment!=null) reviewListFragment.setArguments(bundle);
+
+
+        return view;
+
     }
+
 }
